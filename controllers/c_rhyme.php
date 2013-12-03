21518c5dcb1b812790e8ed13a2bdfcf17a8ecdfd
edhebert <hebert01@gmail.com>
2013-12-03 15:42:39 -0500

    begin implementing the rhyme functionality

--- controllers/c_rhyme.php
+++ controllers/c_rhyme.php
@@ -1,10 +1,5 @@
 <?php
 
-// include access to the Wordnik API in all pages of the site
-require('/wordnik/Swagger.php');
-$myAPIKey = '1aa94b552058e254ba72b0fb2dc0bba89c925fe4037896b95';
-$client = new APIClient($myAPIKey, 'http://api.wordnik.com/v4');
-
 class rhyme_controller extends base_controller {
 
     public function __construct() {
@@ -12,7 +7,38 @@ class rhyme_controller extends base_controller {
     } 
 
     public function index() {
-        echo "This is the index page";
+
+        // include access to the Wordnik API
+        require('/wordnik/Swagger.php');
+        $myAPIKey = '1aa94b552058e254ba72b0fb2dc0bba89c925fe4037896b95';
+        $client = new APIClient($myAPIKey, 'http://api.wordnik.com/v4');
+
+        // API placeholders for random word and rhymes
+        $wordsApi = new WordsApi($client);
+        $wordApi = new WordApi($client);
+
+        // define the minimum number of rhymes a word must have to be accepted
+        define("NUM_RHYMES", 6);
+
+        // loop until we have a word with more than 4 rhymes
+        do {
+            // get random word - exclude proper names and other stuff that isn't fun to rhyme against
+            $result = $wordsApi->getRandomWords($includePartOfSpeech='noun, verb, adjective', $excludePartOfSpeech='family-name, given-name, past-participle, proper-noun, proper-noun-plural, proper-noun-posessive', $sortBy=null, $sortOrder=null, $hasDictionaryDef=true, $minCorpusCount=100000, $maxCorpusCount=null, $minDictionaryCount=null, $maxDictionaryCount=null, $minLength=4, $maxLength=13, $limit=1);
+            
+            $randomWord = $result[0]->word;
+
+            // make sure the word's canonical (no plurals or weird verb tenses)
+            $result = $wordApi->getWord($randomWord, $useCanonical='true', $includeSuggestions=null);
+            $canonical = $result->word;
+
+            // get an array of rhymes for that random word
+            $result = $wordApi->getRelatedWords($canonical, $relationshipTypes='rhyme', $useCanonical=false, $limitPerRelationshipType=1000);
+        } 
+        while (count($result[0]->words) < NUM_RHYMES); 
+
+        $rhymes = $result[0]->words;
+        print($canonical . '<br><br>');
+        print_r($rhymes);
     }    
 
 }
