1b39593fb9c1acf10342858bff190878afed5eb4
edhebert <hebert01@gmail.com>
2013-12-05 15:23:15 -0500

    working on welcome screen

---
+++ js/site.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    // position hero copy
+    $('#herocopy').center();
+
+    // animate  homepage elements (for those with JS)
+    $('h2>span, #playbtn').css('display', 'none');
+
+    // load the "on time" text after a brief delay
+    setTimeout(function() {
+        $('h2>span').fadeIn('slow')
+    }, 1500);
+
+    //fade in the play button
+    setTimeout(function() {
+        $('#playbtn').fadeIn('slow')
+    }, 2500);
+});
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
