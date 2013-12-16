e3ce9ae9ef8654b2c713b443b6becb753ae0d023
edhebert <hebert01@gmail.com>
2013-12-16 16:31:39 -0500

    formatting change to rhyming word

--- views/v_rhyme_index.php
+++ views/v_rhyme_index.php
@@ -1,7 +1,9 @@
 <div id="herocopy">
 </div>
 <div id="gameboard" class="centered">
-    <h2>Word: <span id="word2"></span><br>Score: <span id="score"></span></h2>
+    <h2>Score: <span id="score"></span></h2>
+    <h2><span id="word2"></span></h2>
+        
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <h3>Guesses</h3>
